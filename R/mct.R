d6e1b6561a13992102dc99f965499b46008d2a7f
stineb <benjamin.stocker@gmail.com>
2019-05-01 09:13:52 -0700

    added files

---
+++ R/mct.R
@@ -0,0 +1,44 @@
+mct <- function(df, method="posbal", thresh_deficit=0.1){
+  inst <- tibble()
+  idx <- 0
+  df$deficit <- rep(0, nrow(df))
+  
+  ## search all dates
+  while (idx <= nrow(df)){
+    idx <- idx + 1
+    
+    ## if the water balance (prec-pet*fv) is negative, start accumulating deficit
+    if (df$bal[idx]<0){
+      
+      deficit <- 0
+      if (method=="threshbal") max_deficit <- 0
+      iidx <- idx
+      
+      if (method=="posbal"){
+        ## continue accumulating deficit as long as the water balance is negative
+        while (iidx <= nrow(df) && df$bal[iidx]<0){
+          deficit <- deficit - df$bal[iidx]
+          df$deficit[iidx] <- deficit
+          iidx <- iidx + 1
+        }
+        
+      } else if (method=="threshbal"){
+        ## continue accumulating deficit as long as the deficit is not recuded by more than (thresh_deficit*100) % 
+        while (iidx <= nrow(df) && (deficit - df$bal[iidx] > (1-thresh_deficit) * max_deficit)){
+          deficit <- deficit - df$bal[iidx]
+          if (deficit>max_deficit) max_deficit <- deficit
+          df$deficit[iidx] <- deficit
+          iidx <- iidx + 1
+        }
+      }
+      
+      ## record instance
+      this_inst <- tibble( date_start=df$date[idx], date_end=df$date[iidx-1], deficit=deficit, len=iidx-idx )
+      inst <- inst %>% bind_rows(this_inst)
+      
+      ## update
+      idx <- iidx
+    }
+  }
+  return( list(inst=inst, df=df))
+}
