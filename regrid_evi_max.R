1e4007b0069830780a5a4003bd9629d8e8ef1487
stineb <benjamin.stocker@gmail.com>
2019-06-19 08:23:02 -0700

    completed regrid script

--- regrid_evi_max.R
+++ regrid_evi_max.R
@@ -1,14 +1,17 @@
 library(dplyr)
 library(purrr)
+library(rbeni)
 
-files <- list.files(path = "~/data/modis_monthly-evi/zmaw_data/0_05deg", pattern = "modis_vegetation__LPDAAC__v5__0.05deg", recursive = TRUE )
+dirn <- "/alphadata01/bstocker/data/modis_monthly-evi/zmaw_data/0_05deg/"
+files <- list.files(path = dirn, pattern = "modis_vegetation__LPDAAC__v5__0.05deg", recursive = TRUE )
 
 files <- tibble(filnam = files) %>% 
   dplyr::filter(!str_detect(filnam, "halfdeg")) %>% 
   dplyr::select(filnam) %>% 
   unlist() %>% 
-  unname()
+  unname() %>% 
+  paste0(dirn, .)
 
 purrr::map(
   as.list(files), 
-  regrid_nc(obj = ., varname = "evi", method = "max", outgrid = "halfdeg", returnobj = FALSE))
+  ~regrid_nc(obj = ., varname = "evi", method = "max", outgrid = "halfdeg", returnobj = FALSE))
