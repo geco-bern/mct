ea522e6951ce0ae027880cbcefac5baa59c27fd5
stineb <b.stocker@imperial.ac.uk>
2020-03-24 10:12:52 +0100

    added script to get data for SJ02 sites

--- README.md
+++ README.md
@@ -4,19 +4,52 @@ Steps of my analysis:
 
 ## Steps
 
-### Evaluation
+### Evaluation at FLUXNET sites
 
 1. Extract ET and precip time series data from global files at FLUXNET sites locations (LandEval and WATCH) `rscript_get_data_fluxnetsites.R`
-2. Evaluate ET from LandEval during rain-free periods agains FLUXNET data `rscript_eval_fluxnet2015.R`
+    - [ ]  LandEval data: re-do it, extracting all years' data from 1984 - 2007 (submitted 23.3.2020)
+    - [x]  ALEXI data (submitted 23.3.2020)
+2. Evaluate ET from LandEval during rain-free periods against FLUXNET data `rscript_eval_fluxnet2015.R`
+    - [ ]  LandEval data
+    - [ ]  ALEXI data
 3. Apply MCT method to identify CWD events based on SEBS and precipitation from FLUXNET or WATCH (should yield a list of events for each site)
-4. Extract SiF time series data from global files at FLUXNET sites locations (time series) `not implemented`
-5. Derive k (slope or decay rate of SiF (and observed GPP) vs. CWD and d) at FLUXNET sites locations; try with normalising SiF with PAR
+    - [ ]  Assess CWD time series: linear trend? Plausible end-of-event
+4. Extract SiF and SiF/APAR time series data from global files at FLUXNET sites locations (time series) `not implemented`
+    - [ ]  SiF time series
+    - [ ]  MODIS FPAR time series
+    - [ ]  WATCH-WFDEI SWdown time series
+5. Derive k (slope or decay rate of SiF (and SiF/APAR) and observed GPP (and LUE) vs. CWD and d) at FLUXNET sites locations; do with normalising SiF with APAR or PAR
 6. Estimate rooting depth from CWD statistics (MCT method) → zroot*
 7. Relate k to zroot*
 
-### Application
+### Ecosystem-level zroot
+
+Do steps 1, 3, 4, 5, and 6 for sites in **Schenk & Jackson data**
+
+Evaluation aggregated by biome
+
+- Observed 95% ile rooting depth
+- MCT-estimated rooting depth
+- k_SiF
+
+### Spatial bins zroot
+
+Do steps 1, 3, 4, 5, and 6 for sites in **Shersingh's data**
+
+Evaluation aggregated by biome or spatial bins (~1-2 deg)
+
+- Observed 95% ile rooting depth
+- MCT-estimated rooting depth
+- k_SiF
+
+### Application at global 0.5 deg
 
 Do steps 1, 3, 4, 5, and 6 for each pixel globally.
+
+Visualisation of global maps
+
+- For datasets that are available at 0.05 deg, aggregate to 0.5 deg masking out non-forest pixels at high resolution, and area-weighted mean across remaining hi-res gridcells within lo-res gridcell. Use MODIS land cover data to determine forest cover
+
 
 
 --- Below is old:
--- rscript_get_data_sj02sites.R
+++ rscript_get_data_sj02sites.R
@@ -15,16 +15,15 @@ source("R/extract_points_filelist.R")
 source("R/convert_et.R")
 source("R/align_events.R")
 
-siteinfo <- read_csv("~/data/FLUXNET-2015_Tier1/siteinfo_fluxnet2015_sofun+whc.csv") %>% 
-  rename(sitename = mysitename) %>% 
-  filter(!(classid %in% c("CRO", "WET"))) %>% 
-  filter(year_start<=2007) %>%    # xxx USE ONLY FOR LANDEVAL xxx
-  mutate(date_start = lubridate::ymd(paste0(year_start, "-01-01"))) %>% 
-  mutate(date_end = lubridate::ymd(paste0(year_end, "-12-31")))
+## Get meta info of sites (lon, lat)
+df_sites <- read_csv("~/data/rootingdepth/root_profiles_schenkjackson02/data/root_profiles_D50D95.csv") %>%
+  dplyr::filter(Wetland == "N" & Anthropogenic == "N" & Schenk_Jackson_2002 == "YES") %>% 
+  dplyr::rename(sitename = ID, lat = Latitude, lon = Longitude) %>% 
+  dplyr::mutate(elv = ifelse(elv==-999, NA, elv)) %>% 
+  dplyr::filter(lon!=-999 & lat!=-999) %>% 
+  dplyr::mutate(year_start = 1982, year_end = 2011) %>% 
+  dplyr::select(sitename, lon, lat, elv, year_start, year_end)
 
-df_grid <- siteinfo %>% 
-  dplyr::select(sitename, lon, lat, elv) %>% 
-  dplyr::rename(idx = sitename)
 
 ##------------------------------------------------------------------------
 ## Get data from global fields (WATCH-WFDEI and LandFlux)
@@ -32,7 +31,7 @@ df_grid <- siteinfo %>%
 ## df_grid must contain columns lon, lat, elv, and idx
 
 ## PT-JPL
-filn <- "data/df_pt_jpl.Rdata"
+filn <- "data/df_pt_jpl_sj02.Rdata"
 filn_csv <- str_replace(filn, "Rdata", "csv")
 if (!file.exists(filn)){
   if (!file.exists(filn_csv)){
@@ -65,7 +64,7 @@ if (!file.exists(filn)){
 
 
 ## PM
-filn <- "data/df_pm_mod.Rdata"
+filn <- "data/df_pm_mod_sj02.Rdata"
 filn_csv <- str_replace(filn, "Rdata", "csv")
 if (!file.exists(filn)){
   if (!file.exists(filn_csv)){
@@ -98,7 +97,7 @@ if (!file.exists(filn)){
 
 
 ## SEBS
-filn <- "data/df_sebs.Rdata"
+filn <- "data/df_sebs_sj02.Rdata"
 filn_csv <- str_replace(filn, "Rdata", "csv")
 if (!file.exists(filn)){
   if (!file.exists(filn_csv)){
@@ -129,42 +128,41 @@ if (!file.exists(filn)){
     write_csv(path = "data/df_sebs.csv")
 }
 
-# ##------------------------------------------------------------------------
-# ## WATCH-WFDEI and ALEXI
-# ##------------------------------------------------------------------------
-# filn <- "data/df_alexi.Rdata"
-# filn_csv <- str_replace(filn, "Rdata", "csv")
-# if (!file.exists(filn)){
-#   if (!file.exists(filn_csv)){
-#     df_alexi <- get_data_mct_global(
-#       df_grid, 
-#       dir_et   = "~/data/alexi_tir/netcdf/",        fil_et_pattern   = "EDAY_CERES_", 
-#       dir_prec = "~/data/watch_wfdei/Rainf_daily/", fil_prec_pattern = "Rainf_daily_WFDEI_CRU", 
-#       dir_snow = "~/data/watch_wfdei/Snowf_daily/", fil_snow_pattern = "Snowf_daily_WFDEI_CRU",
-#       dir_temp = "~/data/watch_wfdei/Tair_daily/",  fil_temp_pattern = "Tair_daily_WFDEI",
-#       get_watch = TRUE, get_landeval = FALSE, get_alexi = TRUE,
-#       year_start_watch = 2003, year_end_watch = 2018
-#     )
-#     save(df_alexi, file = filn)
-#     df_alexi %>% 
-#       tidyr::unnest(df) %>%
-#       write_csv(path = filn_csv)
-#   } else {
-#     df_alexi <- read_csv(file = filn_csv) %>% 
-#       group_by(idx, lon, lat) %>% 
-#       tidyr::nest() %>% 
-#       dplyr::mutate(data = purrr::map(data, ~as_tibble(.))) %>% 
-#       dplyr::rename(df = data)
-#   }
-# } else {
-#   load(filn)
-#   df_alexi %>% 
-#     tidyr::unnest(df) %>%
-#     write_csv(path = "data/df_alexi.csv")
-# }
-# 
+##------------------------------------------------------------------------
+## WATCH-WFDEI and ALEXI
+##------------------------------------------------------------------------
+filn <- "data/df_alexi_sj02.Rdata"
+filn_csv <- str_replace(filn, "Rdata", "csv")
+if (!file.exists(filn)){
+  if (!file.exists(filn_csv)){
+    df_alexi <- get_data_mct_global(
+      df_grid,
+      dir_et   = "~/data/alexi_tir/netcdf/",        fil_et_pattern   = "EDAY_CERES_",
+      dir_prec = "~/data/watch_wfdei/Rainf_daily/", fil_prec_pattern = "Rainf_daily_WFDEI_CRU",
+      dir_snow = "~/data/watch_wfdei/Snowf_daily/", fil_snow_pattern = "Snowf_daily_WFDEI_CRU",
+      dir_temp = "~/data/watch_wfdei/Tair_daily/",  fil_temp_pattern = "Tair_daily_WFDEI",
+      get_watch = TRUE, get_landeval = FALSE, get_alexi = TRUE,
+      year_start_watch = 2003, year_end_watch = 2018
+    )
+    save(df_alexi, file = filn)
+    df_alexi %>%
+      tidyr::unnest(df) %>%
+      write_csv(path = filn_csv)
+  } else {
+    df_alexi <- read_csv(file = filn_csv) %>%
+      group_by(idx, lon, lat) %>%
+      tidyr::nest() %>%
+      dplyr::mutate(data = purrr::map(data, ~as_tibble(.))) %>%
+      dplyr::rename(df = data)
+  }
+} else {
+  load(filn)
+  df_alexi %>%
+    tidyr::unnest(df) %>%
+    write_csv(path = "data/df_alexi.csv")
+}
+
 # ## compare
-# ggplot() + 
+# ggplot() +
 #   geom_line(data = df_pt_jpl$df[[29]], aes(x = date, y = et_mm)) +
 #   geom_line(data = df_alexi$df[[70]],  aes(x = date, y = et_mm), col = 'red')
-  
