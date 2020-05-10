bb97a0dae00651e7a12606c6afd5f6b9a200e59e
Benjamin Stocker <benjamin.stocker@gmail.com>
2020-05-10 16:16:06 +0200

    added WTD constraints from Fan et al

--- R/extract_return_level.R
+++ R/extract_return_level.R
@@ -1,6 +1,5 @@
-extract_return_level <- function(out, object, period){
-  
-  df %>% 
+extract_return_level <- function(out, period){
+  out$df_return %>% 
     dplyr::filter(return_period == period) %>% 
     dplyr::pull(return_level)
 }
