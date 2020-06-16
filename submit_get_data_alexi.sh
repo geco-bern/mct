0fc2ead92134ecce84b97d5a78c12df03c791aec
stineb <benjamin.stocker@gmail.comm>
2020-06-16 18:52:51 +0200

    adjusted memory usage for this task

--- submit_get_data_alexi.sh
+++ submit_get_data_alexi.sh
@@ -2,4 +2,4 @@
 
 # bsub -N -o ~/hpc_log/cluster_get_data_alexi.out -W 01:00 -R -n 2 mpirun "rusage[mem=10000]" "R --vanilla --slave < ~/mct/rscript_get_data_alexi.R  >~/hpc_log/rscript_get_data_fluxnetsites.Rout
 
-bsub -n 10 "R --vanilla --slave < ~/mct/rscript_get_data_alexi.R > ~/hpc_log/rscript_get_data_alexi.Rout"
+bsub -n 10 "rusage[mem=5000]" "R --vanilla --slave < ~/mct/rscript_get_data_alexi.R > ~/hpc_log/rscript_get_data_alexi.Rout"
