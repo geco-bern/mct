573613cc7429aa548f50b36d6d77b5634bef2f4b
Benjamin Stocker <benjamin.stocker@gmail.com>
2020-06-18 16:38:37 +0200

    dunno

--- submit_get_data_sif_pk.sh
+++ submit_get_data_sif_pk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bsub -n 5 -W 72:00 -u bestocke -J get_data_sif_pk -R "rusage[mem=5000]" "R --vanilla --slave < ~/mct/rscript_get_data_sif_pk.R > ~/hpc_log/rscript_get_data_sif_pk.Rout"
+bsub -n 5 -W 72:00 -u bestocke -J get_data_sif_pk -R "rusage[mem=2000]" "R --vanilla --slave < ~/mct/rscript_get_data_sif_pk.R > ~/hpc_log/rscript_get_data_sif_pk.Rout"
