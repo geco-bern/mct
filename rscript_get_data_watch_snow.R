6d5f1807f3de6f8808b84a9b3b9f950b4c2563fb
stineb <benjamin.stocker@gmail.comm>
2020-06-18 15:45:44 +0200

    just for testing

--- rscript_get_data_watch_snow.R
+++ rscript_get_data_watch_snow.R
@@ -19,4 +19,4 @@ timenam <- "timestp"
 timedimnam <- "tstep"
 
 ## create files for each longitude slice, containing full time series wrapped for each gridcell (latitude)
-rbeni::nclist_to_df(nclist, outdir, fileprefix, varnam, lonnam, timenam, timedimnam, ncores = 32, single_basedate = FALSE)
+rbeni::nclist_to_df(nclist, outdir, fileprefix, varnam, lonnam, timenam, timedimnam, ncores = 1, single_basedate = FALSE) # 32 cores
