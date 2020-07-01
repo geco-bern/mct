8e00661017179dc4e5d0283d86f7c6dc65a061f9
stineb <benjamin.stocker@gmail.comm>
2020-07-01 14:07:02 +0200

    added writing to file

--- rscript_combine_df_whc.R
+++ rscript_combine_df_whc.R
@@ -1,5 +1,5 @@
 library(dplyr)
-dir <- "~/mct/data/"
+dir <- "~/data/mct_data/df_whc_hires_chunks2/"
 filelist <- paste0(dir, list.files(dir, pattern = "df_whc_hires_chunk_.*.RData"))
 
 list_whc_out <- list()
@@ -8,3 +8,5 @@ for (ifil in filelist){
   list_whc_out[[ifil]] <- df_whc
 }
 df_whc <- bind_rows(list_whc_out)
+
+save(df_whc, file = "~/data/mct_data/df_whc_hires.RData")
