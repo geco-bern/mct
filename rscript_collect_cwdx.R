1ebe7c7ddaf397d3f0bb981ee4c7ed21f4b324c4
Benjamin Stocker <benjamin.stocker@gmail.com>
2020-07-07 18:41:50 +0200

    added files

---
+++ rscript_collect_cwdx.R
@@ -0,0 +1,48 @@
+#!/usr/bin/env Rscript
+
+library(dplyr)
+library(purrr)
+library(tidyr)
+library(magrittr)
+library(multidplyr)
+library(rlang)
+library(lubridate)
+
+source("R/extract_cwdx_byilon.R")
+source("R/collect_cwdx_byilon.R")
+
+## get all available cores
+ncores <- parallel::detectCores()
+
+##------------------------------------------------------------------------
+## 2. collect data from small files into a single dataframe
+##------------------------------------------------------------------------
+nlon <- 7200
+
+if (ncores > 1){
+  
+  cl <- multidplyr::new_cluster(ncores) %>%
+    multidplyr::cluster_assign(collect_cwdx_byilon = collect_cwdx_byilon)
+  
+  ## distribute to cores, making sure all data from a specific site is sent to the same core
+  df <- tibble(ilon = seq(nlon)) %>%
+    multidplyr::partition(cl) %>%
+    dplyr::mutate(data = purrr::map( ilon,
+                                    ~collect_cwdx_byilon(.))) %>% 
+    collect() %>% 
+    tidyr::unnest(data)
+  
+} else {
+  
+  ## testing
+  df <- purrr::map(as.list(seq(nlon)), ~collect_cwdx_byilon(.))
+  
+}
+
+## write to file
+dirn <- "~/mct/data/"
+filn <- paste0("df_cwdx_10_20_40.RData")
+path <- paste0(dirn, filn)
+print(paste("Writing file:", path))
+save(df, file = path)
+
--- rscript_extract_cwdx.R
+++ rscript_extract_cwdx.R
@@ -46,35 +46,3 @@ if (ncores > 1){
   
 }
 
-##------------------------------------------------------------------------
-## 2. collect data from small files into a single dataframe
-##------------------------------------------------------------------------
-nlon <- 7200
-
-if (ncores > 1){
-  
-  cl <- multidplyr::new_cluster(ncores) %>%
-    multidplyr::cluster_assign(collect_cwdx_byilon = collect_cwdx_byilon)
-  
-  ## distribute to cores, making sure all data from a specific site is sent to the same core
-  df <- tibble(ilon = seq(nlon)) %>%
-    multidplyr::partition(cl) %>%
-    dplyr::mutate(data = purrr::map( ilon,
-                                    ~collect_cwdx_byilon(.))) %>% 
-    collect() %>% 
-    tidyr::unnest(data)
-  
-} else {
-  
-  ## testing
-  df <- purrr::map(as.list(seq(nlon)), ~collect_cwdx_byilon(.))
-  
-}
-
-## write to file
-dirn <- "~/mct/data/"
-filn <- paste0("df_cwdx_10_20_40.RData")
-path <- paste0(dirn, filn)
-print(paste("Writing file:", path))
-save(df, file = path)
-
