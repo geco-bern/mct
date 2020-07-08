5e4a0bb28956afb538c500e5e476ede0b0a92ae7
Benjamin Stocker <benjamin.stocker@gmail.com>
2020-07-08 16:52:04 +0200

    2nd round

--- rscript_get_et_mm.R
+++ rscript_get_et_mm.R
@@ -21,26 +21,37 @@ irow_chunk <- split(ilat, ceiling(seq_along(ilat)/nrows_chunk))
 print("getting data for longitude indices:")
 print(irow_chunk[[as.integer(args[1])]]) 
 
-## get all available cores
-ncores <- parallel::detectCores()
+# ## get all available cores
+# ncores <- parallel::detectCores()
+# 
+# if (ncores > 1){
+#   
+#   cl <- multidplyr::new_cluster(ncores) %>%
+#     multidplyr::cluster_library(c("dplyr", "purrr", "tidyr", "dplyr", "magrittr", "rbeni", "ncdf4", "raster", "sp")) %>%
+#     multidplyr::cluster_assign(get_et_mm_byilon = get_et_mm_byilon)
+#     
+# 	## distribute to cores, making sure all data from a specific site is sent to the same core
+# 	df_out <- tibble(ilon = irow_chunk[[as.integer(args[1])]]) %>%
+# 	  multidplyr::partition(cl) %>%
+# 	  dplyr::mutate(out = purrr::map( ilon,
+# 	                                  ~get_et_mm_byilon(.)))
+#     
+# } else {
+#   
+#   df_out <- purrr::map(
+#   	as.list(irow_chunk[[as.integer(args[1])]]), 
+#   	~get_et_mm_byilon(.)
+#   	)
+#   
+# }
 
-if (ncores > 1){
-  
-  cl <- multidplyr::new_cluster(ncores) %>%
-    multidplyr::cluster_library(c("dplyr", "purrr", "tidyr", "dplyr", "magrittr", "rbeni", "ncdf4", "raster", "sp")) %>%
-    multidplyr::cluster_assign(get_et_mm_byilon = get_et_mm_byilon)
-    
-	## distribute to cores, making sure all data from a specific site is sent to the same core
-	df_out <- tibble(ilon = irow_chunk[[as.integer(args[1])]]) %>%
-	  multidplyr::partition(cl) %>%
-	  dplyr::mutate(out = purrr::map( ilon,
-	                                  ~get_et_mm_byilon(.)))
-    
-} else {
-  
-  df_out <- purrr::map(
-  	as.list(irow_chunk[[as.integer(args[1])]]), 
-  	~get_et_mm_byilon(.)
-  	)
-  
-}
+## second round
+load("data/df_file_availability.RData")
+ilon <- df %>% 
+  dplyr::filter(!avl_et_mm) %>% 
+  pull(ilon)
+
+df_out <- purrr::map(
+  as.list(ilon), 
+  ~try(get_et_mm_byilon(.))
+  )
