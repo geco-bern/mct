07cd8ff7a7ae0f702512940195582b531e25a528
Benjamin Stocker <benjamin.stocker@gmail.com>
2020-11-22 13:40:54 +0100

    pasdf

--- rscript_check_files.R
+++ rscript_check_files.R
@@ -80,6 +80,16 @@ check_avail_sif_pk <- function(ilon){
   return(avl)
 }
 
+check_avail_glass <- function(ilon){
+  
+  ## written by R/extract_cwdx_byilon.R
+  dirn <- "~/data/glass/data_tidy/"
+  filn <- paste0("GLASS07B01.V41._ilon_", ilon, ".RData")
+  avl <- file.exists(paste0(dirn, filn))
+  
+  return(avl)
+}
+
 get_ilon_lores <- function(ilon_hires){
   
   lon_lores <- seq(-179.75, 179.75, by = 0.5)
@@ -100,7 +110,8 @@ df <- tibble(ilon = 1:7200) %>%
     # avl_cwdx = check_avail_cwdx(ilon),
     # avl_cwdx_10_20_40 = check_avail_10_20_40(ilon),
     avl_sif_jj = check_avail_sif_jj(ilon),
-    avl_sif_pk = check_avail_sif_pk(ilon)
+    avl_sif_pk = check_avail_sif_pk(ilon),
+    avl_sif_pk = check_avail_glass(ilon)
   )
 
 save(df, file = "./data/df_file_availability.RData")
