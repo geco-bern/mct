3a3f8ecdd403667aac79182473ba21192a60f55c
stineb <benjamin.stocker@gmail.comm>
2020-12-27 18:48:06 +0100

    asdf

--- submit_calc_cwd_et0.sh
+++ submit_calc_cwd_et0.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# requires large memory, finally set to 48 GB
+# requires large memory, finally set to 96 GB
 
 njobs=100
 for ((n=1;n<=${njobs};n++)); do
     echo "Submitting chunk number $n ..."
-    bsub -W 72:00 -u bestocke -J "calc_cwd_et0 $n" -R "rusage[mem=48000]" "Rscript --vanilla rscript_calc_cwd_et0.R $n $njobs"
+    bsub -W 72:00 -u bestocke -J "calc_cwd_et0 $n" -R "rusage[mem=96000]" "Rscript --vanilla rscript_calc_cwd_et0.R $n $njobs"
 done
