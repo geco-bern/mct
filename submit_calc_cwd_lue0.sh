98c571bd6450ebb1353231b1583591c11cf55aff
stineb <benjamin.stocker@gmail.comm>
2021-01-03 20:13:34 +0100

    appropriate submissions

--- submit_calc_cwd_lue0.sh
+++ submit_calc_cwd_lue0.sh
@@ -2,11 +2,11 @@
 
 # requires large memory, finally set to 96 GB
 
-# njobs=100
-# for ((n=1;n<=${njobs};n++)); do
-#     echo "Submitting chunk number $n ..."
-#     bsub -W 72:00 -u bestocke -J "calc_cwd_lue0 $n" -R "rusage[mem=96000]" "Rscript --vanilla rscript_calc_cwd_lue0.R $n $njobs"
-# done
+njobs=100
+for ((n=1;n<=${njobs};n++)); do
+    echo "Submitting chunk number $n ..."
+    bsub -W 72:00 -u bestocke -J "calc_cwd_lue0 $n" -R "rusage[mem=96000]" "Rscript --vanilla rscript_calc_cwd_lue0.R $n $njobs"
+done
 
-## test
-bsub -W 72:00 -u bestocke -J "calc_cwd_lue0 74" -R "rusage[mem=96000]" "Rscript --vanilla rscript_calc_cwd_lue0.R 74 100"
+# ## test
+# bsub -W 72:00 -u bestocke -J "calc_cwd_lue0 74" -R "rusage[mem=96000]" "Rscript --vanilla rscript_calc_cwd_lue0.R 74 100"
--- submit_extract_cwdx.sh
+++ submit_extract_cwdx.sh
@@ -5,5 +5,5 @@
 njobs=50
 for ((n=1;n<=${njobs};n++)); do
     echo "Submitting chunk number $n ..."
-    bsub -W 72:00 -u bestocke -J "extract_cwdx $n" -R "rusage[mem=48000]" "Rscript --vanilla rscript_extract_cwdx.R $n $njobs"   # not all require that much memory
+    bsub -W 72:00 -u bestocke -J "extract_cwdx $n" -R "rusage[mem=72 000]" "Rscript --vanilla rscript_extract_cwdx.R $n $njobs"   # not all require that much memory
 done
