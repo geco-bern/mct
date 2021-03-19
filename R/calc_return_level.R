570f1620b7eca6d7e44e7d9ca6d5172a17afefd3
stineb <b.stocker@imperial.ac.uk>
2021-03-19 14:41:21 +0100

    full on

--- R/calc_return_level.R
+++ R/calc_return_level.R
@@ -12,13 +12,16 @@ calc_return_level <- function(ilon, df_s0){
     mutate(notavl_mod = purrr::map_lgl(mod, ~identical(NA, .))) %>% 
     dplyr::filter(!notavl_mod) %>% 
     dplyr::select(-out_mct) %>%
-    mutate(rp_best = purrr::map2_dbl(s0, mod, ~f_calc_return_level(.x, .y)))
+    mutate(df_rl = purrr::map2(s0, mod, ~f_calc_return_level(.x, .y))) %>% 
+    unnest(df_rl) %>% 
+    mutate(logbias = log(s0 / return_level))
 
 }
 
 f_calc_return_level <- function(x, mod){
 
   return_period <- c(seq(2, 9, by = 1), seq(10, 95, by = 5), seq(100, 300, by = 10), seq(400, 1000, by = 100))
+  
   return_level <- extRemes::return.level(
     mod, 
     return.period = return_period)
@@ -27,10 +30,10 @@ f_calc_return_level <- function(x, mod){
     return_period = return_period, 
     return_level = unname(c(return_level)))
   
-  return_period_best <- df_return_level %>% 
-    dplyr::filter(return_level == return_level[which.min(abs(return_level - x))]) %>% 
-    pull(return_period)
+  # return_period_best <- df_return_level %>% 
+  #   dplyr::filter(return_level == return_level[which.min(abs(return_level - x))]) %>% 
+  #   pull(return_period)
   
-  return(return_period_best)
+  return(df_return_level)
 }
 
