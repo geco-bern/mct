56a3d522dffb0ea520f09c6713b02499e1303ca0
stineb <b.stocker@imperial.ac.uk>
2021-04-10 11:11:40 +0200

    return level work

---
+++ R/collect_rl.R
@@ -0,0 +1,33 @@
+collect_rl_nSIF <- function(ichunk){
+  
+  ## construct output file name
+  dirn <- "~/mct/data/df_rl/"
+  filn <- paste0("df_rl_nSIF_ichunk_", ichunk, "_30.RData")
+  path <- paste0(dirn, filn)
+  
+  if (file.exists(path)){
+    load(path)
+  } else {
+    rlang::inform(paste("File does not exist:", path))
+    df_rl_diag <- tibble()
+  }
+  
+  return(df_rl_diag)
+}
+
+collect_rl_fet <- function(ichunk){
+  
+  ## construct output file name
+  dirn <- "~/mct/data/df_rl/"
+  filn <- paste0("df_rl_fet_ichunk_", ichunk, "_30.RData")
+  path <- paste0(dirn, filn)
+  
+  if (file.exists(path)){
+    load(path)
+  } else {
+    rlang::inform(paste("File does not exist:", path))
+    df <- tibble()
+  }
+  
+  return(df)
+}
