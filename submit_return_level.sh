8d7b1fab0dc8c08c199041a0ca70b6efe6a6c327
stineb <b.stocker@imperial.ac.uk>
2021-04-22 20:41:31 +0200

    return level stuff

--- submit_return_level.sh
+++ submit_return_level.sh
@@ -3,5 +3,6 @@
 njobs=30
 for ((n=1;n<=${njobs};n++)); do
     echo "Submitting chunk number $n ..."
-    bsub -W 72:00 -u bestocke -J "rl $n" -R "rusage[mem=48000]" "Rscript --vanilla rscript_return_level.R $n $njobs"
+    # bsub -W 72:00 -u bestocke -J "rl $n" -R "rusage[mem=48000]" "Rscript --vanilla rscript_return_level.R $n $njobs"
+    Rscript rscript_return_level.R $n $njobs
 done
