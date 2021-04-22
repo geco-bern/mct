a9645c88fc9ae09e25a749b3bc70d241be9443d2
stineb <b.stocker@imperial.ac.uk>
2021-04-22 20:47:34 +0200

    prepared for using nSIF

--- rscript_return_level.R
+++ rscript_return_level.R
@@ -11,10 +11,10 @@ source("R/calc_return_level.R")
 load("data/df_corr.RData")
 
 # load("data/df_corr_fet.RData")  # must use df_corr_fet, and cwd_lue0_fet below
-# # load("data/df_corr_nSIF.RData") # must use df_corr_nSIF, and cwd_lue0_nSIF below
+# load("data/df_corr_nSIF.RData") # must use df_corr_nSIF, and cwd_lue0_nSIF below
 
 df_corr <- df_corr %>% 
-  dplyr::select(lon, lat, s0 = cwd_lue0_fet) %>% ## select which one to consider here!
+  dplyr::select(lon, lat, s0 = cwd_lue0_nSIF) %>% ## select which one to consider here!
   arrange(lon) %>% 
   mutate(idx = 1:n()) %>%
   mutate(chunk = rep(1:as.integer(args[2]), each = (nrow(.)/as.integer(args[2])), len = nrow(.)))
@@ -30,7 +30,8 @@ df_corr_sub <- list_df_split[[as.integer(args[1])]]
 ##------------------------------------------------------------------------
 ## asdf
 ##------------------------------------------------------------------------
-filn <- paste0("data/df_rl/df_rl_fet_ichunk_", args[1], "_", args[2], ".RData")
+# filn <- paste0("data/df_rl/df_rl_fet_ichunk_", args[1], "_", args[2], ".RData")
+filn <- paste0("data/df_rl/df_rl_nSIF_ichunk_", args[1], "_", args[2], ".RData")
 df_rl_diag <- df_corr_sub %>% 
   drop_na() %>% 
   group_by(lon) %>% 
