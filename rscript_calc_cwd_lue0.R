57dc236b0eaf84a994b054bf69d8c2c0e177d3f6
stineb <b.stocker@imperial.ac.uk>
2021-06-06 11:25:18 +0200

    prepared for repeating calc_cwd_lue0

--- rscript_calc_cwd_lue0.R
+++ rscript_calc_cwd_lue0.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-# args <- c(2161, 7200)
+# args <- c(3961, 7200)
 
 library(dplyr)
 library(purrr)
@@ -28,11 +28,15 @@ source("R/calc_cwd_lue0_byilon.R")
 # ilon <- df %>% dplyr::filter(!avl_cwd_lue0) %>% pull(ilon)
 # nlon <- length(ilon)
 
-## this is created in rscript_collect_cwd_lue0.R
-load("data/vec_ilon_missing.RData") # loads vec_ilon_missing
-ilon <- vec_ilon_missing
+# ## third round: this is created in rscript_collect_cwd_lue0.R
+# load("data/vec_ilon_missing.RData") # loads vec_ilon_missing
+# ilon <- vec_ilon_missing
+# nlon <- length(ilon)
+
+## fourth round:
+ilon <- 3961:4176
 nlon <- length(ilon)
-##----
+#----
 
 nchunk <- as.integer(args[2]) # 1000  # make sure this is consistent with the number of parallel jobs (job array!) in the submission script
 nrows_chunk <- ceiling(nlon/nchunk)
