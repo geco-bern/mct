3618f77ea654f948b6a7d48964b3deca0643e299
stineb <b.stocker@imperial.ac.uk>
2021-07-14 13:44:48 +0200

    added up

--- euurs_mct.sh
+++ euurs_mct.sh
@@ -1,1 +1,45 @@
 #!/bin/bash         
+
+source ~/.bash_profile 
+
+euurs df_corr.RData
+euurs gg_fig1a 
+euurs gg_fig1b
+euurs gg_fig1_legend
+euurs 80.RData
+euurs gg_rsip_25.RData
+euurs gg_rsip_50.RData
+euurs gg_rsip_75.RData
+euurs gg_rsip_90.RData
+euurs gg_sj02_10.RData
+euurs gg_sj02_25.RData
+euurs gg_sj02_50.RData
+euurs gg_sj02_75.RData
+euurs gg_sj02_90.RData
+euurs gg_fig1_legend.Rdata
+euurs gg_sif_50.RData
+euurs gg_ef_50.RData
+euurs df_corr.RData
+euurs df_rl_fet.RData
+euurs df_rl_agg_fet.RData
+euurs df_cwdx_10_20_40.RData
+euurs df_cwdx_10_20_80.RData
+euurs df_whc_hires_lasthope.RData
+euurs df_whc.RData
+euurs df_cwd_lue0_2.RData
+euurs df_cwd_et0_3.RData
+euurs df_rivers.RData
+euurs df_sj02_biome_wwf_NEW.Rdata
+euurs siteinfo_sj02.RData
+euurs df_zroot_sj02.RData
+euurs df_mct_merged.RData
+euurs df_mask.RData
+euurs df_zroot80.RData
+euurs cwdx10_tenthdeg.nc
+euurs cwdx20_tenthdeg.nc
+euurs cwdx40_tenthdeg.nc
+euurs cwdx80_tenthdeg.nc
+euurs cwdx100_tenthdeg.nc
+euurs cwdx200_tenthdeg.nc
+euurs gg_fig1c.Rdata
+euurs gg_fig1b.Rdata
