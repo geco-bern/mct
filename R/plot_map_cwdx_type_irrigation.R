b599893e0e9c459f80e89dd07afcabc242b6d6df
Benjamin Stocker <benjamin.stocker@gmail.com>
2021-07-28 08:17:51 +0200

    updated plotting maps

--- R/plot_map_cwdx_type_irrigation.R
+++ R/plot_map_cwdx_type_irrigation.R
@@ -233,8 +233,8 @@ plot_map_cwdx_type_irrigation <- function(
 	  geom_tile(data = df, aes(x = lon, y = lat, fill = type, color = type)) +     # , show.legend = FALSE
 
 	  ## color scale for type, colors from here: https://github.com/cj-holmes/vhs/blob/main/data-raw/create-palettes.R
-	  scale_fill_manual(name = "Flattening", values = "#be7682ff", labels = c("TRUE", "FALSE")) +
-	  scale_color_manual(name = "Flattening", values = "#be7682ff", labels = c("TRUE", "FALSE")) +
+	  scale_fill_manual(name = "Flattening", values = "#eb6133ff", labels = c("TRUE", "FALSE")) +  # magenta: "#be7682ff"; orange: "#eb6133ff", green: "#00902eff"
+	  scale_color_manual(name = "Flattening", values = "#eb6133ff", labels = c("TRUE", "FALSE")) +
 	  
   	## geoms below will use another color scale
 	  new_scale_fill() +
@@ -253,7 +253,7 @@ plot_map_cwdx_type_irrigation <- function(
 	  geom_path(aes(x = long, y = lat, group = group, color = id), data = df_irr_outline, size = 0.3) +
 	  
 	  ## color scale for type, colors from here: https://github.com/cj-holmes/vhs/blob/main/data-raw/create-palettes.R
-	  scale_color_manual(name = "Irrigated", values = "#dc3b39ff", labels = c("Outline", " ")) +
+	  scale_color_manual(name = "Irrigated", values = "#00902eff", labels = c("Outline", " ")) + # green: "#00902eff", red:  "#dc3b39ff"
 
 	  scale_x_continuous(expand = c(0,0)) +
 	  scale_y_continuous(expand = c(0,0)) +
