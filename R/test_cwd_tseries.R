c8cc62ce66a12cb8ce71f7b60c07653d7c9e8afb
Benjamin Stocker <benjamin.stocker@gmail.com>
2022-02-22 10:16:27 +0100

    dunno

--- R/test_cwd_tseries.R
+++ R/test_cwd_tseries.R
@@ -1,4 +1,4 @@
-test_cwd_tseries <- function(out_mct, sitename = NA, filter_years = NA){
+test_cwd_tseries <- function(out_mct, sitename = NA, filter_years = NA, title = NULL){
 
   if ("data.frame" %in% class(out_mct$df)){
     ##------------------------------------------
@@ -17,18 +17,37 @@ test_cwd_tseries <- function(out_mct, sitename = NA, filter_years = NA){
         dplyr::filter(year %in% filter_years)
     }
     
+    # biginstances <- out_mct$inst %>% 
+    #   mutate(year = lubridate::year(date_start)) %>% 
+    #   group_by(year) %>% 
+    #   dplyr::filter(deficit == max(deficit)) %>% 
+    #   pull(iinst)
+    # 
+    # tmp <- out_mct$df %>% 
+    #   dplyr::filter(iinst %in% biginstances)
+    
+    tmp <- out_mct$df %>% 
+      mutate(year = lubridate::year(time)) %>% 
+      group_by(year) %>% 
+      summarise(cwdmax = max(deficit, na.rm = TRUE)) %>% 
+      pull(cwdmax)
+    
+    out_mct$df <- out_mct$df %>% 
+      mutate(deficit_ismax = ifelse(deficit %in% tmp, deficit, NA))
+    
     gg <- ggplot() +
       geom_rect(
         data=out_mct$inst,
         aes(xmin=date_start, xmax=date_end, ymin=-99, ymax=99999),
-        fill=rgb(0,0,0,0.3),
+        fill=rgb(0,0,0,0.2),
         color=NA) +
-      geom_line(data = out_mct$df, aes(date, liquid_to_soil), color="royalblue") +
-      geom_line(data = out_mct$df, aes(date, et_mm), color="springgreen3") +
-      geom_line(data = out_mct$df, aes(date, deficit), color="tomato") +
+      # geom_line(data = out_mct$df, aes(date, liquid_to_soil), color="royalblue") +
+      # geom_line(data = out_mct$df, aes(date, et_mm), color="springgreen3") +
+      geom_line(data = out_mct$df, aes(time, deficit), color="tomato") +
+      geom_point(data = out_mct$df, aes(time, deficit_ismax)) +
       coord_cartesian(ylim=c(0, maxval)) +
       theme_classic() +
-      labs(subtitle = "ET: ALEXI, precipitation: WATCH-WFDEI", x = "Date", y = "Cumulative water deficit (mm)")
+      labs(subtitle = title, x = "Date", y = "CWD (mm)")
     
     if (!is.na(sitename)){
       gg <- gg +
